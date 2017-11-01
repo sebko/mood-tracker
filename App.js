a493fd0e935ec8f620d6ee5ac98cb4f90c36c9ea
Sebastian Key <sebastianwkey@gmail.com>
2017-11-01 21:34:57 +1100

    add apollo

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://api.graph.cool/simple/v1/cj9fjxkri31v50134v97azi7u',
+  }),
+  cache: new InMemoryCache(),
+});
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ApolloProvider client={client}>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+      </ApolloProvider>
     );
   }
 }
